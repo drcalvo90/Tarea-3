44d0a135cda5836fec9e182c25db8df852cabba4
drcalvo90 <javier.calvo@uhispano.ac.cr>
2021-06-18 21:49:28 -0600

    second commit

--- src/uh/ac/cr/InteractiveControl.java
+++ src/uh/ac/cr/InteractiveControl.java
@@ -16,13 +16,14 @@ public class InteractiveControl {
 
 
         // void method, so it show options to the user
-    public void scannerSelection(){
+    public void inputSelection (){
         System.out.println("How many times you Wish to Print hello world on the screen");
         optionSelected= scanner.nextInt();
 
     }
-    for (optionSelected=constantNumber;optionSelected<= numberOperator;optionSelected++){
+    for (numberOperator=0;numberOperator< optionSelected;numberOperator++){
+        for (constantNumber=0;constantNumber<optionSelected;constantNumber++){
         System.out.println("hello world");
     }
     }
-}
+
